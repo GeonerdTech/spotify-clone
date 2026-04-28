885f3682d348b981ab9de1166bef086d77cd6643
GeonerdTech <geonerdtechnical@gmail.com>
2026-04-28 08:22:27 +0000

    update

--- spotify-clone/src/components/Navbar.jsx
+++ spotify-clone/src/components/Navbar.jsx
@@ -14,8 +14,25 @@ const Navbar = () => {
             <img onClick={()=>navigate(1)} className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_right} alt="" />
         </div>
         <div className='flex items-center gap-4'>
-            <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'>Explore Premium</p>
-            <p className='bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer'>Install App</p>
+            <p
+  onClick={() =>
+    window.open("https://www.spotify.com/premium/", "_blank")
+  }
+  className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'
+>
+  Explore Premium
+</p>
+            <p
+  onClick={() =>
+    window.open(
+      "https://play.google.com/store/apps/details?id=com.spotify.music",
+      "_blank"
+    )
+  }
+  className='bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer'
+>
+  Install App
+</p>
             <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center'>G</p>
         </div>
       </div>
